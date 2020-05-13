c26db31b954fbbc877383398d5b2ab85c4e044b5
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-12 20:56:26 -0700

    achieved minimum working code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello!
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { secretWord: '', letterGuessed: '', revealedWord: '', bodyPartsRemaining: '' }
+    props.dispatch({ type: 'SET_SECRET_WORD', secretWord: 'abracadabra' })
+  }
+
+  handleGuessClick = event => {
+    this.props.dispatch({ type: 'SUBMIT_LETTER', letterGuessed: document.getElementById('input').value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {`Secret Word: ${this.props.revealedWord}`}
+          <br />
+          {`Body Parts Remaining: ${this.props.bodyPartsRemaining ? this.props.bodyPartsRemaining.join(', ') : ''}`}
+        </div>
+        <input id='input'></input>
+        <button onClick={this.handleGuessClick}>Guess</button>
+      </div>
+    );
+  }
+
 }
 
 const mapStateToProps = state => {
