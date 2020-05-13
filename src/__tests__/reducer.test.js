4ea63699613847d7266ebd106a5a9595f8f6258c
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-12 19:17:30 -0700

    test passes for submitting a correct letter

--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -5,9 +5,14 @@ describe('reducer tests', () => {
   test('set secret word', () => {
     const action = { type: 'SET_SECRET_WORD', secretWord: 'abracadabra' };
     const state = reducer({}, action);
-    expect(state).toEqual({ secretWord: 'abracadabra', letterGuessed: '', bodyPartsRemaining: ['head', 'torso', 'left arm', 'right arm', 'left hand', 'right hand', 'left leg', 'right leg', 'left foot', 'right foot'] });
+    expect(state).toEqual({ secretWord: 'abracadabra', letterGuessed: '', revealedWord: '', bodyPartsRemaining: ['head', 'torso', 'left arm', 'right arm', 'left hand', 'right hand', 'left leg', 'right leg', 'left foot', 'right foot'] });
   });
 
-
+  test('submitting a correct letter', () => {
+    const action = { type: 'SUBMIT_LETTER', letterGuessed: 'a' };
+    let state = { secretWord: 'abracadabra', letterGuessed: '', revealedWord: '', bodyPartsRemaining: ['head', 'torso', 'left arm', 'right arm', 'left hand', 'right hand', 'left leg', 'right leg', 'left foot', 'right foot'] };
+    state = reducer(state, action);
+    expect(state).toEqual({ secretWord: 'abracadabra', letterGuessed: 'a', revealedWord: '', bodyPartsRemaining: ['head', 'torso', 'left arm', 'right arm', 'left hand', 'right hand', 'left leg', 'right leg', 'left foot', 'right foot'] });
+  });
 
 });
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,14 +1,17 @@
 export default (state = {}, action) => {
-  const { type, secretWord, letterGuessed, bodyPartsRemaining } = action;
+  const { type, secretWord, letterGuessed, revealedWord, bodyPartsRemaining } = action;
   switch (type) {
     case 'SET_SECRET_WORD':
       let newState = state;
       newState.secretWord = secretWord;
       newState.letterGuessed = '';
+      newState.revealedWord = '';
       newState.bodyPartsRemaining = ['head', 'torso', 'left arm', 'right arm', 'left hand', 'right hand', 'left leg', 'right leg', 'left foot', 'right foot'];
       return newState;
     case 'SUBMIT_LETTER':
-      return state;
+      let newState2 = state;
+      newState2.letterGuessed = letterGuessed;
+      return newState2;
     default:
       return state;
   }
